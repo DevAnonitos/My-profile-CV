3e4fac4065487750313f819868d02c7d03ada4d0
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2022-10-19 00:07:09 +0700

    update-home-page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,24 +1,59 @@
 
+import { useState, useEffect } from 'react';
+
 import { Link } from 'react-router-dom';
+
+import AnimatedLetters from '../AnimatedLetters';
 
 // style components
 import './Home.scss'
 
 function Home() {
+    // logic state
+    const [letterClass, setLetterClass] = useState('text-animate');
+    const nameArray = ['B', 'a', 'o', 'N', 'g', 'u', 'y', 'e', 'n'];
+    const jobArray = ['w', 'e', 'b', 'd', 'e', 'v', 'e' ,'l', 'o', 'p', 'e', 'r'];
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+    }, [])
+
+
     return ( 
         <div className='container home-page'>
             <div className='text-zone'>
-                <h1 className='main-title'>Hi, <br/>I'm
-                    BaoNguyen
+
+                <h1 className='main-title'>
+                    {/* title */}
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i,</span>
+                    <br/>
+                    <span className={`${letterClass} _13`}>I</span>
+                    <span className={`${letterClass} _14`}>'m</span>
+                    
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={nameArray}
+                        idx={15}
+                    />
                     <br />
-                    web developer
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={jobArray}
+                        idx={22}
+                    />
                 </h1>
+
                 <h2 className='sub-title'>
                     Fullstack developer 
                     / Javascript Expert 
                     / Blogger
                 </h2>
+
                 <Link to='/contact' className='flat-button'>Contact me</Link>
+
             </div>
         </div>
     );
