9134c2c23c76876eaff8d17d93e0d3dfe970c49e
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2022-10-20 17:27:25 +0700

    update-about-layout

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,10 @@
+// components
+import { faAngular, faHtml5, faCss3, faReact, faJsSquare, faGitAlt } from '@fortawesome/free-brands-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+// logic
 import React, { useState, useEffect } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
+import Loader from 'react-loaders'
 import './About.scss'
 
 const About = () => {
@@ -12,46 +17,49 @@ const About = () => {
     }, [])
 
   return (
-    <div className='container about-page'>
-        <div className='text-zone'>
-            <h1 className='main-title'>
-                <AnimatedLetters
-                    letterClass={letterClass}
-                    strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
-                    idx={15}
-                />
-            </h1>
-            <p className='main-description'>
-                I'm studying HoChiMinh Open university in 2021. My major's computer science. A day by day, I always going on an up to date the latest technologies. In the near future, I will become a full stack developer from Zero to Hero.
-            </p>
-            <p className='main-description'>
-                I'm a persistent, patient and hard-working person. I want to make a useful contribution to the community.
-            </p>
-        </div>
+    <>
+        <div className='container about-page'>
+            <div className='text-zone'>
+                <h1 className='main-title'>
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+                        idx={15}
+                    />
+                </h1>
+                <p className='main-description'>
+                    I'm studying HoChiMinh Open university in 2021. My major's computer science. A day by day, I always going on an up to date the latest technologies. In the near future, I will become a full stack developer from Zero to Hero.
+                </p>
+                <p className='main-description'>
+                    I'm a persistent, patient and hard-working person. I want to make a useful contribution to the community.
+                </p>
+            </div>
 
-        <div className='stage-cube-cont'>
-            <div className='cube-spinner'>
-                <div className='face1'>
-
-                </div>
-                <div className='face2'>
-
-                </div>
-                <div className='face3'>
-
-                </div>
-                <div className='face4'>
-
-                </div>
-                <div className='face5'>
-
-                </div>
-                <div className='face6'>
-
+            <div className='stage-cube-cont'>
+                <div className='cube-spinner'>
+                    <div className='face1'>
+                        <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+                    </div>
+                    <div className='face2'>
+                        <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+                    </div>
+                    <div className='face3'>
+                        <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+                    </div>
+                    <div className='face4'>
+                        <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+                    </div>
+                    <div className='face5'>
+                        <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+                    </div>
+                    <div className='face6'>
+                        <FontAwesomeIcon icon={faGitAlt} color="#EC4" />
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+        <Loader type="pacman" />
+    </>
   )
 }
 
