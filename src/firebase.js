f6ceb2b07492d647f8e16d98eb4a3da20d989735
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2022-11-21 23:12:11 +0700

    update-src@2

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,8 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { getStorage } from "firebase/storage";
 import { getFirestore } from 'firebase/firestore/lite'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
   apiKey: "AIzaSyB6Yn7h0JtfSzCCXlaysL86yp1IQlaoBxs",
