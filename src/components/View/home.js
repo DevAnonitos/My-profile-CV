d90f4a1d17bfefecfd3b208382ca2ea116d49af5
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2022-11-21 08:41:50 +0700

    update-log

--- src/components/View/home.js
+++ src/components/View/home.js
@@ -2,6 +2,38 @@ import React, { useRef } from 'react'
 import { auth, db, storage } from '../../firebase'
 
 import { Typography, AppBar, Card, CardActions, CardContent, CssBaseline, Grid, Toolbar, Container, Button, CardMedia } from '@mui/material'
+import Avatar from '@mui/material/Avatar';
+
+function stringToColor(string) {
+  let hash = 0;
+  let i;
+
+  /* eslint-disable no-bitwise */
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  let color = '#';
+
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.slice(-2);
+  }
+  /* eslint-enable no-bitwise */
+
+  return color;
+}
+
+function stringAvatar(name) {
+  return {
+    sx: {
+      bgcolor: stringToColor(name),
+    },
+    children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+  };
+}
+
+
 
 
 const HoMe = () => {
@@ -13,6 +45,7 @@ const HoMe = () => {
         <Typography variant="h2" color="initial">
           My Current Projects
         </Typography>
+        <Avatar {...stringAvatar('Kent Dodds')} sx={{marginLeft: 2}} />
         <button className='Logout-btn' onClick={() => auth.signOut()}>Sign out</button>
       </Toolbar>
     </AppBar> 
